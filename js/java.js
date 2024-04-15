bb69ffc7a0c376809375615c117bebc5ce654af0
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-14 23:03:46 -0600

    definiciones de asincronia

--- js/java.js
+++ js/java.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+//asincronismo:
+//los lenguajes son sincronos: tarea por tarea. 
+//single-threaded.  
+//bloqueante: la tarea no devuelve el control hasta que sea completado.
+//no bloqueante: una tarea devuelve el control. si se completo devuelve los datos.
+//sincrono; las tareas que se ejecutan de forma secuencal. deben esperar a quelas otras terminen.
+//asincrono: las tareas pueden ser mas tarde. 
+//concurrencia; loop de eventos.
+
+//callbacks. funcion de otra funcion. 
+
+//promesas: no bloqueante. van a cumplor ahora, en el futuro. o nunca.
+
+//async. / await. 
+
+//js; multihilo. muchas tareas. 
