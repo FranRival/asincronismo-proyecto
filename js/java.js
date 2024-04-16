2311bae777ba27779dd4fbdefc0050d833c501a1
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-15 21:45:58 -0600

    0, comenzando configrando el proyecto.

--- js/java.js
+++ js/java.js
@@ -1,15 +1,3 @@
 console.log("hello");
 
-
-//EVENT LOOP.
- //blucle, patron de diseno que distriuye eventos en un programa. 
- //memory heat; bojetos, funciones, elementos, destinados a un espacio de memoria gigante. en espera de ser utilizados.
-
- //call stack; pila. forma organizada. 
-
- //last in, last out. 
- //task queue; cola de tareas. concurrencia. para las tareas que ya estan listas. 
- //microtask queue; promesas de otra forma de ejecutarse. con prioridad o sin.
- //web apis; settimeout. xmlhttprequest. dom.
-
- //event loop; parte de la logica. del task queue al stack. 
+//configuracion + documentacion. + herramienta: fake api + 
