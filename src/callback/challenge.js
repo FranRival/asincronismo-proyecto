d6cb8ebb9185246c9c52b32d9f40c8817eeca232
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-17 23:40:17 -0600

    el error estaba en la linea 27, donde tenia categories. y no category

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,7 +1,7 @@
 //fetch data
 
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const API = 'https://api.escuelajs.co/api/v1' 
+const API = 'https://api.escuelajs.co/api/v1'; 
 
 function fetchData(urlApi,callback){
     let xhttp = new XMLHttpRequest()
@@ -20,11 +20,11 @@ function fetchData(urlApi,callback){
     xhttp.send()
 }
 
-fetchData(`${API}/products`, function(error1, data1){
+fetchData(`${API}/products`, function(error1, data1){//probando codigo
     if (error1) return console.error(error1)
     fetchData(`${API}/products/${data1[0].id}`, function(error2 , data2){
         if (error2) return console.error(error2)
-        fetchData(`${API}/categories/${data2?.categories?.id}`, function (error3, data3){
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3){
             if (error3) return console.error(error3)
             console.log(data1[0]);
             console.log(data2.title)
