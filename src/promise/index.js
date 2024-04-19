f109891ec7dccc2b95f5b57de81a7d353ec0ed29
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-18 21:28:19 -0600

    de la linea 12 a la 24. apenas y logro understand this modofoko shit

--- src/promise/index.js
+++ src/promise/index.js
@@ -5,8 +5,20 @@ const promise = new Promise(function (resolve, reject){
     resolve ('hey!')
 })
 
-const cow = 15
+
+
+
+
+const cow = 15;
 
 const countCows = new Promise (function (resolve, reject){
-    
+    if (cow > 10) {
+        resolve(`Tenemos las vaquitas necesarias. Total ${cow}`)
+    }else {
+        reject('No contamos con las vaquillas alv')
+    }    
+})
+
+countCows.then((result) => {
+    console.log(result);
 })
