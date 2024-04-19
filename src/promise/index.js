25ab1ac476ad266ef33939f35520860457e657f2
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-18 21:22:58 -0600

    ejemplo de promesa en funcion

---
+++ src/promise/index.js
@@ -0,0 +1,12 @@
+//va a pasar. ahora, manana, o nunca.
+//una promesa tiene 3 estados; pendiente, cumplido, rechazado.
+
+const promise = new Promise(function (resolve, reject){
+    resolve ('hey!')
+})
+
+const cow = 15
+
+const countCows = new Promise (function (resolve, reject){
+    
+})
